e7477ee4e113560a97f3b0615e442e199c24a9a2
Mael Police <police@et.esiea.fr>
2021-11-29 15:37:22 +0100

    boucle

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,12 +1,15 @@
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Bienvenue");
+
         java.util.Scanner entree =   new java.util.Scanner(System.in);
         String phrase = entree.nextLine();
 
-        if(!phrase.equals("quit"))
+        while(!phrase.equals("quit"))
         {
             System.out.println("Unknown command");
+
+            phrase = entree.nextLine();
         }
     }
 }
